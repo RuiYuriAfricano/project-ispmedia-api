3f36a39013a0c2b7a7d31b6c23d622bd25714542
RuiYuriAfricano <yuriafricano03@gmail.com>
2024-06-15 11:36:06 +0100

    feat: add conteudoDosGrupos.controller

---
+++ src/conteudoDosGrupos/conteudoDosGrupos.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Put, Delete, Body, Param, ParseIntPipe } from '@nestjs/common';
+import { ConteudoDosGruposService } from './conteudoDosGrupos.service';
+import { AddConteudoDosGruposDto } from './dto/addConteudoDosGruposDto';
+import { UpdateConteudoDosGruposDto } from './dto/updateConteudoDosGruposDto';
+
+@Controller('conteudo-dos-grupos')
+export class ConteudoDosGruposController {
+  constructor(private conteudoDosGruposService: ConteudoDosGruposService) { }
+
+  @Post()
+  add(@Body() data: AddConteudoDosGruposDto) {
+    return this.conteudoDosGruposService.add(data);
+  }
+
+  @Put()
+  update(@Body() data: UpdateConteudoDosGruposDto) {
+    return this.conteudoDosGruposService.update(data);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.conteudoDosGruposService.remove(id);
+  }
+
+  @Get(':id')
+  getOne(@Param('id', ParseIntPipe) id: number) {
+    return this.conteudoDosGruposService.getOne(id);
+  }
+
+  @Post('listarConteudoDosGrupos')
+  listarConteudoDosGrupos() {
+    return this.conteudoDosGruposService.listarConteudoDosGrupos();
+  }
+}

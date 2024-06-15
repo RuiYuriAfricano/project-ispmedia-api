cf078015f94e19e33c1c328859ac580e15573bde
RuiYuriAfricano <yuriafricano03@gmail.com>
2024-06-15 11:34:31 +0100

    feat: add dto conteudoDosGrupos

---
+++ src/conteudoDosGrupos/dto/addConteudoDosGruposDto.ts
@@ -0,0 +1,6 @@
+export class AddConteudoDosGruposDto {
+    fkGrupoDeAmigos: number;
+    fkAlbum?: number;
+    fkMusica?: number;
+    fkVideo?: number;
+}
---
+++ src/conteudoDosGrupos/dto/updateConteudoDosGruposDto.ts
@@ -0,0 +1,7 @@
+export class UpdateConteudoDosGruposDto {
+    codConteudo: number;
+    fkGrupoDeAmigos?: number;
+    fkAlbum?: number;
+    fkMusica?: number;
+    fkVideo?: number;
+}

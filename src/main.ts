7f75e53478d357aca8b1983ac7537956bf3f9157
RuiYuriAfricano <yuriafricano03@gmail.com>
2024-06-21 10:04:24 +0100

    feat: atualizacao do cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  //app.enableCors();
+  // Configuração do CORS
+  app.enableCors({
+    origin: 'http://localhost:3000', // URL da origem permitida
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
+    credentials: true, // Define se as credenciais (como cookies) podem ser enviadas
+  });
   await app.listen(3333);
   //await app.listen(3333, '0.0.0.0');
 }

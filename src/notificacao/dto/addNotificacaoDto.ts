2a314f1dae715142b8a75914c6aeeb37042b43ca
RuiYuriAfricano <yuriafricano03@gmail.com>
2024-06-24 02:26:30 +0100

    chore: update dto

--- src/notificacao/dto/addNotificacaoDto.ts
+++ src/notificacao/dto/addNotificacaoDto.ts
@@ -1,4 +1,5 @@
 export class AddNotificacaoDto {
     textoNotificacao: string;
     fkUtilizador: number;
+    utilizadorOrigem?: string;
 }
--- src/notificacao/dto/updateNotificacaoDto.ts
+++ src/notificacao/dto/updateNotificacaoDto.ts
@@ -2,4 +2,5 @@ export class UpdateNotificacaoDto {
     codNotificacao: number;
     textoNotificacao: string;
     fkUtilizador: number;
+    utilizadorOrigem?: string;
 }

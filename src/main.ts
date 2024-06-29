e17b0ce14e15ecb21980d2ed4e6a98ba1c906784
RuiYuriAfricano <yuriafricano03@gmail.com>
2024-06-29 14:08:55 +0100

    chore: config https

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import * as path from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // Configurar as opções HTTPS
+  const httpsOptions = {
+    key: fs.readFileSync(path.join(__dirname, '/../certificado/key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, '/../certificado/cert.pem')),
+  };
+
+  // Criar a aplicação Nest com HTTPS
+  const app = await NestFactory.create(AppModule, { httpsOptions });
 
   //app.enableCors();
   // Configuração do CORS
   app.enableCors({
-    origin: 'http://localhost:3000', // URL da origem permitida
+    origin: 'https://localhost:3000', // URL da origem permitida
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
     allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
     credentials: true, // Define se as credenciais (como cookies) podem ser enviadas

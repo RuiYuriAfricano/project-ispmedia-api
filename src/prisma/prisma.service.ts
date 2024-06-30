17b71917243c4dcb2fc6fb6eeb1b24ba68c12ed2
RuiYuriAfricano <yuriafricano03@gmail.com>
2024-06-30 17:26:25 +0100

    chore: confi middleware soft delete

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -5,6 +5,31 @@ import { PrismaClient } from '@prisma/client';
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
+    this.$use(async (params, next) => {
+      const readActions = ['findMany', 'findFirst',];
+      const deleteActions = ['delete', 'deleteMany'];
+
+      if (readActions.includes(params.action)) {
+        if (params.args?.where) {
+          if (params.args.where.deletedAt === undefined) {
+            params.args.where['deletedAt'] = null;
+          }
+        } else {
+          params.args = { ...params.args, where: { deletedAt: null } };
+        }
+      }
+
+      if (deleteActions.includes(params.action)) {
+        params.action = params.action === 'delete' ? 'update' : 'updateMany';
+        if (params.args?.data) {
+          params.args.data['deletedAt'] = new Date();
+        } else {
+          params.args = { ...params.args, data: { deletedAt: new Date() } };
+        }
+      }
+
+      return next(params);
+    });
   }
 
   async enableShutdownHooks(app: INestApplication) {
